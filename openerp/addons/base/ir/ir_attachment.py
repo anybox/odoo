e00ad6b7a6fa4fde091d0c1a7378635fe2971deb
Christophe Combelles <ccomb@free.fr>
2015-04-05 17:14:13 +0300

    Revert "[FIX] #4309 Attachments with res_id=0 should not be considered orphaned". Obsoleted by eaad70d
    This reverts commit 40df25cbd55445e84b73b6cca18f955bb80de60f.

--- openerp/addons/base/ir/ir_attachment.py
+++ openerp/addons/base/ir/ir_attachment.py
@@ -200,11 +200,10 @@ class ir_attachment(osv.osv):
                 ids = [ids]
             cr.execute('SELECT DISTINCT res_model, res_id FROM ir_attachment WHERE id = ANY (%s)', (ids,))
             for rmod, rid in cr.fetchall():
-                if not (rmod and rid is not False):
+                if not (rmod and rid):
                     require_employee = True
                     continue
-                if rid:
-                    res_ids.setdefault(rmod,set()).add(rid)
+                res_ids.setdefault(rmod,set()).add(rid)
         if values:
             if values.get('res_model') and values.get('res_id'):
                 res_ids.setdefault(values['res_model'],set()).add(values['res_id'])
